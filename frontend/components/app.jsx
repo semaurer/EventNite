45d770df0d4f911b3ab72bdae11d3dc39170f0f3
semaurer <semaurer14@gmail.com>
2016-12-07 17:22:46 -0500

    Implement dropdown nav styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -37,32 +37,43 @@ class App extends React.Component {
 
   loggedInEls () {
     return (
-      [
-        <li key="312" className='header-nav-item'>
-          { this.props.currentUser.fname }</li>,
-        <dropdown key="310" className="profile-dropdown">
-          <li key="313" className="prof-dropdown-b">Tickets 0</li>
-          <li key="314" className="prof-dropdown-b">Saved 0</li>
-          <li key="315" className="prof-dropdown-b">Managed Events</li>
-          <li key="316" className='header-nav-item'
-          onClick={ this.props.logOut }>Log Out</li>
-        </dropdown>
-      ]
-    )
+      <nav className='header-nav'>
+        <h2 className='header-nav-item-logo'>EventNite</h2>
+        <ul className='header-items'>
+          <li className='header-nav-item'>Browse Events</li>
+          <li className='header-nav-item user-name'>
+            { this.props.currentUser.fname }
+            <ul className="profile-dropdown">
+              <li className="prof-dropdown-b">Tickets 0</li>
+              <li className="prof-dropdown-b">Saved 0</li>
+              <li className="prof-dropdown-b">Manage Events</li>
+              <li className='prof-dropdown-b'
+              onClick={ this.props.logOut }>Log Out</li>
+            </ul>
+          </li>
+          <li className='header-nav-item-c'>Create Event</li>
+        </ul>
+      </nav>
+    );
   }
 
   loggedOutEls () {
     return (
-      [
-        <li onClick={ this.openModal.bind(this, false) }
-          key="321" className='header-nav-item'>
-          <div key="323">Sign Up</div>
-        </li>,
-        <li onClick={ this.openModal.bind(this, true) }
-          key="322" className='header-nav-item'>
-          <div key="324">Log In</div>
-        </li>
-      ]
+      <nav className='header-nav'>
+        <h2 className='header-nav-item-logo'>EventNite</h2>
+        <ul className='header-items'>
+          <li className='header-nav-item'>Browse Events</li>
+          <li onClick={ this.openModal.bind(this, false) }
+            className='header-nav-item'>
+            <div>Sign Up</div>
+          </li>,
+          <li onClick={ this.openModal.bind(this, true) }
+            className='header-nav-item'>
+            <div>Log In</div>
+          </li>
+          <li className='header-nav-item-c'>Create Event</li>
+        </ul>
+      </nav>
     )
   }
 
@@ -76,30 +87,18 @@ class App extends React.Component {
     }
 
     return (
-      <section className='homepage-container group'>
-        <header className='header-main'>
-          <nav className='header-nav'>
-            <h2 className='header-nav-item-logo'>EventNite</h2>
-            <ul className='header-items'>
-              <li className='header-nav-item'>Browse Events</li>
-              { sessionItems }
-              <li className='header-nav-item-c'>Create Event</li>
-            </ul>
-          </nav>
-        </header>
-
+      <header className='headeulr-main'>
+        { sessionItems }
         <Modal style={ ModalStyle } contentLabel="" isOpen={ this.state.modalOpen }
-          onRequestClose={ this.closeModal }>
+            onRequestClose={ this.closeModal }>
           <button className="auth-modal-b" onClick={ this.closeModal }>X</button>
           <SessionModalForm errors={ this.props.errors }
             logIn={ this.props.logIn } signUp={ this.props.signUp }
             formType={ this.state.signIn } router={ this.props.router }
             closeModal={ this.closeModal } swapSignInState={ this.swapSignInState }/>
         </Modal>
-
-
         {this.props.children}
-      </section>
+      </header>
     );
   }
 }
