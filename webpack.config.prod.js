53a205584f225666f1ac8db5de9ad12cd160a78a
semaurer <semaurer14@gmail.com>
2017-11-19 21:28:53 -0500

    Move to env and stage-2 to allow for new es6 translations

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -30,7 +30,7 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'env', 'stage-2']
         }
       }
     ]
