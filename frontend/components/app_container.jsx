62b02cc0492f36f66e7964639e93498992994c18
semaurer <semaurer14@gmail.com>
2017-11-24 12:48:38 -0500

    Clean up formatting

--- frontend/components/app_container.jsx
+++ frontend/components/app_container.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import App from './app';
-import { signUp, logIn,
-  logOut, clearErrors } from '../actions/session_actions';
+import { signUp, logIn, logOut, clearErrors } from '../actions/session_actions';
 import { receiveSearch, resetSearch } from '../actions/search_actions';
 import { clearEvent, clearEvents } from '../actions/event_actions';
 
 const mapStateToProps = ({ session }) => {
-
   return {
     loggedIn: Boolean(session.currentUser),
     errors: session.errors,
@@ -16,17 +14,16 @@ const mapStateToProps = ({ session }) => {
 };
 
 const mapDispatchToProps = (dispatch) => {
-
-    return {
-      signUp: (user) => dispatch(signUp(user)),
-      logIn: (user) => dispatch(logIn(user)),
-      logOut: () => dispatch(logOut()),
-      clearEvent: () => dispatch(clearEvent()),
-      clearEvents: () => dispatch(clearEvents()),
-      receiveSearch: (search) => dispatch(receiveSearch(search)),
-      resetSearch: () => dispatch(resetSearch()),
-      clearErrors: () => dispatch(clearErrors()),
-    };
+  return {
+    signUp: (user) => dispatch(signUp(user)),
+    logIn: (user) => dispatch(logIn(user)),
+    logOut: () => dispatch(logOut()),
+    clearEvent: () => dispatch(clearEvent()),
+    clearEvents: () => dispatch(clearEvents()),
+    receiveSearch: (search) => dispatch(receiveSearch(search)),
+    resetSearch: () => dispatch(resetSearch()),
+    clearErrors: () => dispatch(clearErrors()),
+  };
 };
 
 export default connect(
