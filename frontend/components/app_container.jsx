f48b6b6eec59de2427f2403e9f997594af589e84
semaurer <semaurer14@gmail.com>
2017-11-24 22:48:57 -0500

    Remove props that got added to session modal container

--- frontend/components/app_container.jsx
+++ frontend/components/app_container.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import App from './app';
-import { signUp, logIn, logOut, clearErrors } from '../actions/session_actions';
+import { logOut, clearErrors } from '../actions/session_actions';
 import { receiveSearch, resetSearch } from '../actions/search_actions';
 import { clearEvent, clearEvents } from '../actions/event_actions';
 
 const mapStateToProps = ({ session }) => {
   return {
     currentUser: session.currentUser,
-    errors: session.errors,
     curPath: window.location.hash,
     loggedIn: Boolean(session.currentUser),
   };
@@ -19,15 +18,10 @@ const mapDispatchToProps = (dispatch) => {
     clearErrors: () => dispatch(clearErrors()),
     clearEvent: () => dispatch(clearEvent()),
     clearEvents: () => dispatch(clearEvents()),
-    logIn: (user) => dispatch(logIn(user)),
     logOut: () => dispatch(logOut()),
     receiveSearch: (search) => dispatch(receiveSearch(search)),
     resetSearch: () => dispatch(resetSearch()),
-    signUp: (user) => dispatch(signUp(user)),
   };
 };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
