aefa07c292be38bf39d9cd69e958b7ffa879ea97
semaurer <semaurer14@gmail.com>
2017-11-24 23:36:27 -0500

    Modify path and remove unecessary prop

--- frontend/components/app_container.jsx
+++ frontend/components/app_container.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import App from './app';
-import { logOut, clearErrors } from '../actions/session_actions';
+import { logOut, clearErrors, swapModalDisplay } from '../actions/session_actions';
 import { receiveSearch, resetSearch } from '../actions/search_actions';
 import { clearEvent, clearEvents } from '../actions/event_actions';
 
 const mapStateToProps = ({ session }) => {
   return {
     currentUser: session.currentUser,
-    curPath: window.location.hash,
-    loggedIn: Boolean(session.currentUser),
+    curPath: window.location.hash.slice(1),
   };
 };
 
@@ -21,6 +20,7 @@ const mapDispatchToProps = (dispatch) => {
     logOut: () => dispatch(logOut()),
     receiveSearch: (search) => dispatch(receiveSearch(search)),
     resetSearch: () => dispatch(resetSearch()),
+    swapModalDisplay: (displayingSignUpForm) => dispatch(swapModalDisplay(displayingSignUpForm)),
   };
 };
 
