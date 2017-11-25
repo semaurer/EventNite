5ad5018b7c0b2a7300fecacdd3ea2e3f2c222863
semaurer <semaurer14@gmail.com>
2017-11-24 22:49:07 -0500

    Add session modal container

---
+++ frontend/components/session/sessionModalContainer.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import SessionModalForm from './session_modal_form';
+import { clearErrors, logIn, signUp, swapModalDisplay } from '../../actions/session_actions';
+
+const mapStateToProps = ({ session }) => {
+  return {
+    displayingSignUpForm: session.displayingSignUpForm,
+    errors: session.errors,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearErrors: () => dispatch(clearErrors()),
+    logIn: (user) => dispatch(logIn(user)),
+    signUp: (user) => dispatch(signUp(user)),
+    swapModalDisplay: (displayingSignUpForm) => dispatch(swapModalDisplay(displayingSignUpForm)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionModalForm)
