8783214fd45646aed1dbac97e04f9face4b0b379
semaurer <semaurer14@gmail.com>
2017-12-16 13:47:00 -0500

    Make category switch and use more semantic naming and take logic out of render

--- frontend/components/events/eventIndex.jsx
+++ frontend/components/events/eventIndex.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router';
 
 class EventIndex extends React.Component {
-  state = { categoryOpen: "closed" };
+  state = { categoryMenu: "closed" };
 
   componentWillReceiveProps (nextProps) {
     if (this.props.location.pathname.includes("categories")) {
       if (nextProps.location.pathname === "events") {
         this.props.fetchEvents();
-        this.setState({ categoryOpen: "closed" });
+        this.setState({ categoryMenu: "closed" });
       }
     }
   }
@@ -19,7 +19,7 @@ class EventIndex extends React.Component {
   }
 
   returnToFull = () => {
-    this.setState({ categoryOpen: "full" });
+    this.setState({ categoryMenu: "full" });
     this.props.router.push("/events/categories");
     this.props.fetchEvents();
   }
@@ -90,44 +90,51 @@ class EventIndex extends React.Component {
         parentCat = category;
       }
     });
-    this.setState({ categoryOpen: "subSelected" });
+    this.setState({ categoryMenu: "subSelected" });
     this.props.router.push(`events/categories/${parentCat.name}/${childCat.name}`);
     this.props.removeEvents();
     this.props.categoryFilterFetchEvents(e.currentTarget.id);
   }
 
   configureMenuForCats = (e) => {
-    this.setState({ categoryOpen: "parentSelected" });
+    this.setState({ categoryMenu: "parentSelected" });
     this.props.router.push(`events/categories/${e.currentTarget.innerText}`);
     this.props.removeEvents();
     this.props.categoryFilterFetchEvents(e.currentTarget.id);
   }
 
   changeMenuState = (e) => {
-    if (this.state.categoryOpen === "closed") {
+    if (this.state.categoryMenu === "closed") {
       this.props.resetSearch();
-      this.setState({ categoryOpen: "full" });
+      this.setState({ categoryMenu: "full" });
       this.props.router.push("events/categories");
     } else {
-      this.setState({ categoryOpen: "closed" });
+      this.setState({ categoryMenu: "closed" });
       this.props.router.push("/events");
       this.props.removeEvents();
       this.props.fetchEvents();
     }
   }
 
-  fullMenuEls = () => {
-    const fullMenuEls = [<div className="all-c"
-      key="cat-index"><h4>All Categories</h4></div>];
-    this.props.categories.forEach(category => {
-      if (category.parent_category_id === null) {
-        fullMenuEls.push(<div key={ category.id }
-          className="non-active"><h4
-          onClick={ this.configureMenuForCats }
-          id={ category.id }>{ category.name }</h4></div>);
+  renderFullCategoryMenu = () => {
+    const { categories } = this.props;
+
+    const allCategories = categories.map((category) => {
+      if (!category.parent_category_id) {
+        return (
+          <div key={ category.id } className="non-active">
+            <h4 onClick={ this.configureMenuForCats } id={ category.id }>{ category.name }</h4>
+          </div>
+        );
       }
     });
-    return fullMenuEls;
+
+    return (
+      <div className="all-c" key="cat-index">
+        <h4>All Categories</h4>
+        { allCategories }
+      </div>
+    );
   }
 
   updateSavedStatus = (e) => {
@@ -144,6 +151,14 @@ class EventIndex extends React.Component {
     } else {
       this.props.saveEvent(e.currentTarget.id);
     }
+  }
+
+  determineHeader = () => {
+    const { events, search } = this.props;
+    if (search) {
+      return events.length ? `${search} events` : `Sorry, we couldn't find any ${search} events`
+    }
+    return "Events for you"
   }
 
   renderEvents = (savedEvents) => {
@@ -193,20 +208,23 @@ class EventIndex extends React.Component {
     });
   }
 
+  renderCategories = () => {
+    const { categoryMenu } = this.state;
+
+    switch (categoryMenu) {
+      case 'full':
+        return this.renderFullCategoryMenu();
+      case 'parentSelected':
+        return this.parentCatMenu();
+      case 'subSelected':
+        return this.subCatMenu();
+      default:
+        return null;
+    }
+  }
+
   render () {
     const { events, savedEvents, search } = this.props;
-
-    let fullMenu = "";
-    if (this.state.categoryOpen === "full") fullMenu = this.fullMenuEls();
-    if (this.state.categoryOpen === "parentSelected") fullMenu = this.parentCatMenu();
-    if (this.state.categoryOpen === "subSelected") fullMenu = this.subCatMenu();
-    let searchResponse = "Events For You";
-
-    if (events.length && search) {
-      searchResponse = `${this.props.search} events`;
-    } else if (!events.length && search) {
-      searchResponse = `Sorry, we couldn't find any ${this.props.search} events`;
-    }
 
     return (
       <div className="browse-page group">
@@ -214,11 +232,11 @@ class EventIndex extends React.Component {
           <aside>
             <div className="category-selector">
               <h3 onClick={ this.changeMenuState }>CATEGORY</h3>
-              { fullMenu }
+              { this.renderCategories() }
             </div>
           </aside>
           <div className="events">
-            <h2>{ searchResponse }</h2>
+            <h2>{ this.determineHeader() }</h2>
             <ul>
               { !events.length && !search ? <div className="loader" /> : null }
               { this.renderEvents(savedEvents) }
