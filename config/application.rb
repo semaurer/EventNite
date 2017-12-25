2ba8cc63a203c1a9d647d6436e235b22c0ab74de
semaurer <semaurer14@gmail.com>
2017-12-25 13:00:38 -0500

    Resolve host name for storage

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,12 @@ module EventNite
     config.paperclip_defaults = {
       :storage => :s3,
       :compression => { :png => '-optimize', :jpeg => '-optimize' },
-      :s3_region => ENV["s3_region"],
+      s3_host_name: "s3-#{ENV['s3_region']}.amazonaws.com",
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
         :secret_access_key => ENV["s3_secret_access_key"],
+        :s3_region => ENV["s3_region"],
       }
     }
   end
