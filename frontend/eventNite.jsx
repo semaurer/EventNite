9619799e62f75de9af2b62ebbc3ec836bd67299e
semaurer <semaurer14@gmail.com>
2017-12-25 22:46:29 -0500

    convert to camelCase and remove redundant else statement

--- frontend/eventNite.jsx
+++ frontend/eventNite.jsx
@@ -6,14 +6,12 @@ import Modal from 'react-modal';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
+
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
     store = configureStore(preloadedState);
-  } else {
-    store = configureStore();
   }
 
-  const root = document.getElementById('root');
   Modal.setAppElement(document.body);
-  ReactDOM.render(<Root store={ store }/>, root);
+  ReactDOM.render(<Root store={ store }/>, document.getElementById('root'));
 });
